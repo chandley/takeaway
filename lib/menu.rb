b87b4818099c778bac22848d3e119b7fd8e6c5da
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-11-07 13:09:55 +0000

    add menu show

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,5 +1,17 @@
-class Menu 
-  def dishes
-    {'Kippers' => 10}
+class Menu
+  attr_reader :dishes
+
+  def initialize
+  @dishes =  {'Kippers' => 10, 'Porridge' => 5, 'Bacon sandwich' => 10}
   end
+
+  def show
+    puts 'Bill of Fayre'
+      dishes.each do | name, price |
+        puts "#{name}".ljust(25) + price.to_s.rjust(3)
+      end
+    end
 end
+
+menu = Menu.new
+menu.show
